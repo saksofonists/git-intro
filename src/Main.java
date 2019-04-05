28226733b5aec002248ce8e896eab6d587ff2e82
Toms Kusiņš <t.kusins@gmail.com>
2019-04-05 18:56:15 +0300

    Task 2 partial

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int read = sc.nextInt();
         System.out.println("Hello, Tom!");
     }
 }
